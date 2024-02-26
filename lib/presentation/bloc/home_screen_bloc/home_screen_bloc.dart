31f199c16081aea6e4153ae86c26061a72e9ccb5
CodingCloud79 <160732070+CodingCloud79@users.noreply.github.com>
2024-02-26 19:00:25 +0530

    Bloc in HomeScreen

--- lib/presentation/bloc/home_screen_bloc/home_screen_bloc.dart
+++ lib/presentation/bloc/home_screen_bloc/home_screen_bloc.dart
@@ -1,11 +1,60 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'home_screen_event.dart';
 part 'home_screen_state.dart';
 
 class HomeScreenBloc extends Bloc<HomeScreenEvent, HomeScreenState> {
-  HomeScreenBloc() : super(HomeScreenInitial()) {
-    
+  HomeScreenBloc() : super(HomeScreenInitialState()) {
+    on<NavigateToHomeTabEvent>(navigateToHomeTabEvent);
+    on<NavigateToLearnTabEvent>(navigateToLearnTabEvent);
+    on<NavigateToEarnTabEvent>(navigateToEarnTabEvent);
+    on<NavigateToMyContactsTabEvent>(navigateToMyContactsTabEvent);
+    on<NavigateToMyProfileEvent>(navigateToMyProfileEvent);
+    on<NavigateToPrivacyPolicyEvent>(navigateToPrivacyPolicyEvent);
+    on<NavigateToRewardPointsEvent>(navigateToRewardPointsEvent);
+    on<NavigateToWithdrawlsEvent>(navigateToWithdrawlsEvent);
   }
+
+  FutureOr<void> navigateToHomeTabEvent(
+      NavigateToHomeTabEvent event, Emitter<HomeScreenState> emit) {
+    emit(NavigateToHomeTabState());
+    debugPrint(" Bloc :  NavigateToHomeTabState");
+  }
+
+  FutureOr<void> navigateToWithdrawlsEvent(
+      NavigateToWithdrawlsEvent event, Emitter<HomeScreenState> emit) {
+    emit(NavigateToWithdrawlsState());
+    debugPrint(" Bloc :  NavigateToWithdrawlsState");
+  }
+
+  FutureOr<void> navigateToRewardPointsEvent(
+      NavigateToRewardPointsEvent event, Emitter<HomeScreenState> emit) {
+    emit(NavigateToRewardPointsState());
+    debugPrint(" Bloc :  NavigateToRewardPointsState");
+  }
+
+  FutureOr<void> navigateToPrivacyPolicyEvent(
+      NavigateToPrivacyPolicyEvent event, Emitter<HomeScreenState> emit) {
+    emit(NavigateToPrivacyPolicyState());
+    debugPrint(" Bloc :  NavigateToPrivacyPolicyState ");
+  }
+
+  FutureOr<void> navigateToMyProfileEvent(
+      NavigateToMyProfileEvent event, Emitter<HomeScreenState> emit) {
+        emit(NavigateToMyProfileTabState());
+            debugPrint(" Bloc : NavigateToMyProfileTabState ");
+      }
+
+  FutureOr<void> navigateToMyContactsTabEvent(
+      NavigateToMyContactsTabEvent event, Emitter<HomeScreenState> emit) {}
+
+  FutureOr<void> navigateToEarnTabEvent(
+      NavigateToEarnTabEvent event, Emitter<HomeScreenState> emit) {}
+
+  FutureOr<void> navigateToLearnTabEvent(
+      NavigateToLearnTabEvent event, Emitter<HomeScreenState> emit) {}
 }
--- lib/presentation/bloc/home_screen_bloc/home_screen_state.dart
+++ lib/presentation/bloc/home_screen_bloc/home_screen_state.dart
@@ -1,6 +1,29 @@
 part of 'home_screen_bloc.dart';
 
 @immutable
-sealed class HomeScreenState {}
+abstract class HomeScreenState {}
 
-final class HomeScreenInitial extends HomeScreenState {}
+abstract class HomeScreenActionState extends HomeScreenState {}
+
+class HomeScreenInitialState extends HomeScreenState {}
+
+class HomeScreenLoadingState extends HomeScreenState {}
+
+class HomeScreenLoadedState extends HomeScreenState {}
+
+class HomeScreenErrorState extends HomeScreenState {}
+
+class NavigateToHomeTabState extends HomeScreenActionState {}
+
+class NavigateToLearnTabState extends HomeScreenActionState {}
+
+class NavigateToEarnTabState extends HomeScreenActionState {}
+
+class NavigateToMyContactsTabState extends HomeScreenActionState {}
+
+class NavigateToWithdrawlsState extends HomeScreenActionState {}
+class NavigateToRewardPointsState extends HomeScreenActionState {}
+
+class NavigateToPrivacyPolicyState extends HomeScreenActionState {}
+
+class NavigateToMyProfileTabState extends HomeScreenActionState {}
